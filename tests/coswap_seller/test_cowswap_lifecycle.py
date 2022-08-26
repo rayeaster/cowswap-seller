6d649724ab2dc42d4f311a2da5dd5eff3d69e69e
rayeaster <rayeaster@hotmail.com>
2022-08-26 11:03:31 +0800

    add test for fix increaseAllowance

--- tests/coswap_seller/test_cowswap_lifecycle.py
+++ tests/coswap_seller/test_cowswap_lifecycle.py
@@ -1,5 +1,6 @@
 import brownie
 from brownie import *
+import pytest
 
 from scripts.send_order import get_cowswap_order
 
@@ -35,6 +36,7 @@ def test_verify_and_add_order(seller, usdc, weth, manager):
     Cowswap RELAYER approval amount override issue found by watchpug audit 
     https://www.hacknote.co/17c261f7d8fWbdml/doc/182cf5567f8Gdn7B#182cf5567f8Gdn7B
 """
+@pytest.mark.skip(reason="existing deployed version will override")
 def test_approval_override(seller_v_0_3_deployed, usdc, weth, wbtc, seller_v_0_3_deployed_manager):
   sell_amount = 500 * 1000000000000000000
   settlement = interface.ICowSettlement(seller_v_0_3_deployed.SETTLEMENT())
@@ -60,6 +62,30 @@ def test_approval_override(seller_v_0_3_deployed, usdc, weth, wbtc, seller_v_0_3
   ## According to Cowswap relayer code, the first order will fail to be filled due to insufficient allowance to transfer sell_token
   ## https://etherscan.io/address/0xc92e8bdf79f0507f65a392b0ab4667716bfe0110#code#F15#L112  
   assert weth.allowance(seller_v_0_3_deployed.address, seller_v_0_3_deployed.RELAYER()) == sell_amount2
+  
+def test_approval_override_fix(seller, usdc, weth, wbtc, manager):
+  sell_amount = 500 * 1000000000000000000
+  settlement = interface.ICowSettlement(seller.SETTLEMENT())
+
+  ## First order to setup inital approval to Cowswap relayer 
+  order_details = get_cowswap_order(seller, weth, usdc, sell_amount)
+  data = order_details.order_data
+  uid = order_details.order_uid
+  assert uid == seller.getOrderID(data)
+  seller.initiateCowswapOrder(data, uid, {"from": manager})
+  assert settlement.preSignature(uid) > 0
+  assert weth.allowance(seller.address, seller.RELAYER()) == sell_amount
+
+  ## Second order to override smaller approval to Cowswap relayer 
+  sell_amount2 = 100 * 1000000000000000000
+  order_details2 = get_cowswap_order(seller, weth, wbtc, sell_amount2)
+  data2 = order_details2.order_data
+  uid2 = order_details2.order_uid
+  assert uid2 == seller.getOrderID(data2)
+  seller.initiateCowswapOrder(data2, uid2, {"from": manager})
+  assert settlement.preSignature(uid2) > 0
+  
+  assert weth.allowance(seller.address, seller.RELAYER()) == sell_amount2 + sell_amount
   
   
 
